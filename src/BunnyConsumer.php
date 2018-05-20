f6b016915038ab22c4add78ee22f06da60633a1e
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-05-20 23:56:24 +0200

    Reworked to take everything down once a shutdown occurs

--- src/BunnyConsumer.php
+++ src/BunnyConsumer.php
@@ -5,6 +5,8 @@ namespace ReactiveApps\Command\BunnyConsumer;
 use Bunny\Async\Client;
 use React\EventLoop\LoopInterface;
 use ReactiveApps\Command\Command;
+use ReactiveApps\Rx\Shutdown;
+use WyriHaximus\React\ObservableBunny\Message;
 use WyriHaximus\React\ObservableBunny\ObservableBunny;
 
 final class BunnyConsumer implements Command
@@ -17,19 +19,43 @@ final class BunnyConsumer implements Command
     private $loop;
 
     /**
-     * @param LoopInterface $loop
+     * @var Shutdown
      */
-    public function __construct(LoopInterface $loop)
+    private $shutdown;
+
+    /**
+     * @param LoopInterface $loop
+     * @param Shutdown $shutdown
+     */
+    public function __construct(LoopInterface $loop, Shutdown $shutdown)
     {
         $this->loop = $loop;
+        $this->shutdown = $shutdown;
     }
 
     public function __invoke()
     {
-        $bunny = new Client($this->loop);
+        /** @var Client $bunny */
+        $bunny = yield (new Client($this->loop))->connect();
         $observableBunny = new ObservableBunny($this->loop, $bunny);
-        $observableBunny->consume('queue:name', [0, 10])->subscribe(function (Message $message) {
+        $subject = $observableBunny->consume('queue:name', [0, 10])->subscribe(function (Message $message) {
             // Handle message
+        });
+
+        /**
+         * Dispose of the subscription
+         */
+        $this->shutdown->subscribe(null, null, function () use ($subject) {
+            $subject->dispose();
+        });
+
+        /**
+         * Give observable bunny a second to clean up
+         */
+        $this->shutdown->subscribe(null, null, function () use ($bunny) {
+            $this->loop->addTimer(1, function () use ($bunny) {
+                $bunny->disconnect();
+            });
         });
     }
 }
